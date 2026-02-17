c981d62b5565ac55d11d3f101f551f8f982d3c04
tk5323-web <tk5323@srmist.edu.in>
2026-02-17 16:38:33 +0530

    Add OOPSBannerApp_UC2 class with new banner output

--- OOPSBannerApp (2).java
+++ OOPSBannerApp (2).java
@@ -1,11 +1,12 @@
-public class OOPSBannerApp {
+public class OOPSBannerApp_UC2 {
+
     public static void main(String[] args) {
-        System.out.println("  *****    *****    *****    *****  ");
-        System.out.println(" *     *  *     *  *     *  *       ");
-        System.out.println(" *     *  *     *  *     *  *       ");
-        System.out.println(" *     *  *     *  *     *   *****  ");
-        System.out.println(" *     *  *     *  *             *  ");
-        System.out.println(" *     *  *     *  *             *  ");
-        System.out.println("  *****    *****    *****    *****  ");
+        System.out.println("   ***   "+" "+"   ***   "+" "+"******   "+" "+"    *****");
+        System.out.println(" **   ** "+" "+" **   ** "+" "+"**    ** "+" "+" ***     ");
+        System.out.println("**     **"+" "+"**     **"+" "+"**     **"+" "+"***      ");
+        System.out.println("**     **"+" "+"**     **"+" "+"**   **  "+" "+"   ***   ");
+        System.out.println("**     **"+" "+"**     **"+" "+"******   "+" "+"      ***");
+        System.out.println(" **   ** "+" "+" **   ** "+" "+"**       "+" "+"     *** ");
+        System.out.println("   ***   "+" "+"   ***   "+" "+"**       "+" "+"*****    ");
     }
 }
